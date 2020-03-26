bfc9946146285a053168ef5ec1d5eb464984f11d
zerkijulian92 <zerkijuliandri92@gmail.com>
2020-03-26 11:51:44 +0700

    (Bagian 10) - 3. Page Home Bagian 2

--- app/src/main/java/com/takatutustudio/lat04movie/MainActivity.kt
+++ app/src/main/java/com/takatutustudio/lat04movie/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.takatutustudio.lat04movie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var datalist = ArrayList<FilmModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_movie.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        datalist.add(
+            FilmModel(
+                "",
+                "",
+                "",
+                "",
+                R.color.colorAccent,
+                0,
+                0.0f
+            )
+        )
+        rv_movie.adapter = MovieAdapter(datalist){
+
+        }
     }
 }
---
+++ app/src/main/java/com/takatutustudio/lat04movie/MovieAdapter.kt
@@ -0,0 +1,55 @@
+package com.takatutustudio.lat04movie
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class MovieAdapter (private var data: List<FilmModel>,
+                    private val listener:(FilmModel) -> Unit)
+    : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+
+    lateinit var ContextAdapter : Context
+
+    // onCreateViewHolder
+    // -> Inisialisasi untuk view layout item_movie_horizontal
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        ContextAdapter     = parent.context
+
+        val inflatedView : View = layoutInflater.inflate(R.layout.item_movie_horizontal, parent, false)
+        return MovieViewHolder(inflatedView)
+    }
+
+    // size data yang dilakukan untuk perulanggan
+    override fun getItemCount(): Int = data.size
+
+    // onBindViewHolder
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        // Menghubugkan dan memanggil class binItem
+        holder.binItem(data[position], listener, ContextAdapter, position)
+    }
+
+    // Inisialisasi data FilmModel
+    // MovieViewHolder
+    // -> Mengabungkan class MovieViewHolder dengan MovieAdapter ini
+    class MovieViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        private val ivPoster:ImageView = view.findViewById(R.id.iv_poster)
+        private val tvTitle:TextView   = view.findViewById(R.id.tv_title)
+
+        // Menampilkan data yang ada di FilmModel
+        fun binItem(data: FilmModel, listener: (FilmModel) -> Unit, context: Context, position:Int) {
+            tvTitle.text = data.judul
+            Glide.with(context)
+                .load(data.poster)
+
+            itemView.setOnClickListener {
+                listener(data)
+            }
+        }
+    }
+}
